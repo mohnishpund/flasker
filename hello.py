252c29b23f253f4665fd6f8528a1d1bec2720978
mohnish Pund <mohnishpund11@gmail.com>
2022-06-21 03:01:54 -0700

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+
+#creta a flask instance
+
+app = Flask(__name__)
+#create a route decoder
+
+
+
+@app.route('/')
+
+#def index():
+#	return "<h1>Hello World!</h1>"
+
+
+def index():
+	first_name = "mohnish"
+	stuff = "This is bold text"
+	return render_template("index.html", 
+		first_name=first_name,
+		stuff=stuff)
+
+
+
+#localhost:5000/user/mohnish
+
+@app.route('/user/<name>')
+
+def user(name):
+	return render_template("user.html", user_name=name)
+
+
+#create Error page
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
+
+# Invalid URL
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template("500.html"), 500
